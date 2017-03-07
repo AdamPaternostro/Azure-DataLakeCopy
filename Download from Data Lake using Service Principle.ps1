9bb624f7016dbfcf168ac54069c0b13f8e7884ff
Adam Paternostro <AdamPaternostro@users.noreply.github.com>
2017-03-07 10:27:03 -0500

    Add files via upload

---
+++ Download from Data Lake using Service Principle.ps1
@@ -0,0 +1,25 @@
+﻿# Data Lake items
+$fileNameToUpload = "Invoice-1215752192.csv"
+$dataLakePath = "/Spark-Data/" + $fileNameToUpload
+$fileToDownload="C:\Users\guest\Desktop\Invoice1\" + $fileNameToUpload
+$sourceDataLakeAccount = "sampledatalakestore"
+
+# Login Items
+$tenant = "<<REMOVED>>"
+$clientKey = "<<REMOVED>>"
+$clientId = "<<REMOVED>>"
+$subscriptionId = "<<REMOVED>>"
+
+# Login an select subscription
+$secpasswd = ConvertTo-SecureString $clientKey -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential ($clientId, $secpasswd)
+Login-AzureRmAccount -ServicePrincipal -Tenant $tenant  -Credential $mycreds
+Select-AzureRmSubscription -SubscriptionId $subscriptionId
+ 
+# Download from Data Lake
+$write =  "BEGIN Download: " + $fileToDownload
+Write-Output $write
+Export-AzureRmDataLakeStoreItem -AccountName $destinationDataLakeAccount -Path $dataLakePath -Destination $fileToDownload -Force
+$write =  "END   Download: " + $fileToDownload
+Write-Output $write
+
